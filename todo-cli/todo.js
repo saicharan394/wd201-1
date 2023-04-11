c677ff9fb71197882525a0f8fa55871569c9712e
cHAnU172-9 <109171958+cHAnU172-9@users.noreply.github.com>
2023-04-11 22:53:50 +0530

    Add files via upload

---
+++ todo-cli/todo.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter(
+      (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
+    );
+  };
+
+  const dueToday = () => {
+    return all.filter(
+      (item) => item.dueDate === new Date().toLocaleDateString("en-CA")
+    );
+  };
+
+  const dueLater = () => {
+    return all.filter(
+      (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
+    );
+  };
+
+  const toDisplayableList = (list) => {
+    const completionStatus = item.completed ? "[x]" : "";
+    const displayedDate =
+      item.dueDate === new Date().toISOString().slice(0, 10)
+        ? ""
+        : item.dueDate;
+    return list.map(
+      (item) => $(completionStatus) + $(item.title) + $(displayedDate)
+    );
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+module.exports = todoList;
